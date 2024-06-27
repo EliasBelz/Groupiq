5a7f3bc2c7188b61b9504308ed3c77c84134ec3e
marinawooden <mwoode@uw.edu>
2024-06-26 21:59:45 -0700

    better chat view

---
+++ groupiq_flutter/lib/models/message.dart
@@ -0,0 +1,20 @@
+// This class represents a single message from a user on the service
+class Message {
+  final int id;
+  final String text;
+  final String posterName;
+  final int posterId;
+  final bool isAdmin;
+  final DateTime timePosted;
+  final Map<String, int> reactions; // this'll be like ":-)": 12
+
+  Message(
+      {required this.id,
+      required this.text,
+      required this.posterName,
+      required this.posterId,
+      required this.isAdmin,
+      DateTime? timePosted,
+      this.reactions = const {}})
+      : this.timePosted = timePosted ?? DateTime.now();
+}
--- groupiq_flutter/lib/views/chat_view.dart
+++ groupiq_flutter/lib/views/chat_view.dart
@@ -1,8 +1,11 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:community_material_icon/community_material_icon.dart';
 import 'package:flutter/material.dart';
+import 'package:groupiq_flutter/models/message.dart';
 import 'package:groupiq_flutter/widgets/chat_card/chat_avatar.dart';
 import 'package:groupiq_flutter/widgets/chat_top_nav.dart';
+import 'package:intl/intl.dart';
+import 'dart:math' as math;
 
 class ChatView extends StatefulWidget {
   const ChatView({super.key});
@@ -12,39 +15,67 @@ class ChatView extends StatefulWidget {
 }
 
 class _ChatViewState extends State<ChatView> {
-  List<String> messages = [
-    "WHAT",
-    "Message 1 is a very long message.  What ",
-    "How about other characters??? Другие языки???  我说中文很好."
+  List<Message> messages = [
+    Message(
+      id: 1,
+      text: "Hello",
+      posterName: "Xx_fishlover_xX",
+      posterId: 123457,
+      isAdmin: false,
+    ),
+    Message(
+      id: 2,
+      text: "This is a longer message.  I think it's nice.",
+      posterName: "marina",
+      posterId: 123456,
+      isAdmin: false,
+    ),
   ];
 
   void addMessage(String message) {
+    Message newMessage = Message(
+        id: message.length + 1,
+        text: message,
+        posterName: "marina",
+        posterId: 123456,
+        isAdmin: false);
+
     setState(() {
-      messages.add(message);
+      messages.add(newMessage);
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: ChatTopNav(),
-        body: Column(
-          children: [
-            Expanded(
-              child: ListView.builder(
-                itemCount: messages.length,
-                itemBuilder: (context, index) {
-                  return MessageWidget(message: messages[index]);
-                },
+      appBar: const ChatTopNav(),
+      body: Column(
+        children: [
+          Flexible(
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+              ),
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: messages.length,
+                  itemBuilder: (context, index) {
+                    return MessageWidget(message: messages[index]);
+                  },
+                ),
               ),
             ),
-            InputBar(
-              onSend: (message) {
-                addMessage(message);
-              },
-            ),
-          ],
-        ));
+          ),
+          InputBar(
+            onSend: (message) {
+              addMessage(message);
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
 
@@ -59,6 +90,7 @@ class InputBar extends StatefulWidget {
 
 class _InputBarState extends State<InputBar> {
   final TextEditingController _controller = TextEditingController();
+  bool isTyping = false;
 
   void _send() {
     if (_controller.text.isNotEmpty) {
@@ -68,53 +100,119 @@ class _InputBarState extends State<InputBar> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    _controller.addListener(() {
+      setState(() {
+        isTyping = _controller.text.isNotEmpty;
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(8.0),
-      color: Colors.grey[200],
-      child: Row(
-        children: [
-          Expanded(
-            child: TextField(
-              controller: _controller,
-              decoration: InputDecoration(
-                hintText: "Enter your message",
+        padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+        decoration: const BoxDecoration(
+            color: Colors.white,
+            border: Border(
+                top: const BorderSide(width: 1.0, color: Colors.black26))),
+        child: SafeArea(
+          child: Row(
+            children: [
+              IconButton(
+                icon: const Icon(Icons.gif_box,
+                    color: Colors.black54, size: 30.0),
+                onPressed: () {
+                  print("Post Gif");
+                },
               ),
-            ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: TextField(
+                    controller: _controller,
+                    decoration: const InputDecoration(
+                      isDense: true,
+                      contentPadding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                      filled: true,
+                      fillColor: Color.fromARGB(255, 232, 238, 249),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(color: Colors.black26, width: 1.0),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(color: Colors.black26, width: 1.0),
+                      ),
+                      hintText: "Enter your message",
+                    ),
+                  ),
+                ),
+              ),
+              Transform.rotate(
+                angle: -30 * math.pi / 180,
+                child: IconButton(
+                  icon: Icon(
+                    CommunityMaterialIcons.send_circle_outline,
+                    size: 30.0,
+                    color: isTyping ? Colors.blue : Colors.black26,
+                  ),
+                  onPressed: _send,
+                ),
+              )
+            ],
           ),
-          IconButton(
-            icon: Icon(Icons.send),
-            onPressed: _send,
-          ),
-        ],
-      ),
-    );
+        ));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
 
 class MessageWidget extends StatelessWidget {
-  final String message;
+  final Message message;
 
   MessageWidget({required this.message});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-      child: Align(
-        alignment: Alignment.centerLeft,
+        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
         child: Container(
-          padding: EdgeInsets.all(12.0),
-          decoration: BoxDecoration(
-            color: Colors.blueAccent,
-            borderRadius: BorderRadius.circular(8.0),
-          ),
-          child: Text(
-            message,
-            style: TextStyle(color: Colors.white),
-          ),
-        ),
-      ),
-    );
+            padding: const EdgeInsets.all(12.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(width: 1.0, color: Colors.black26),
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: RichText(
+                      text: TextSpan(
+                          style: const TextStyle(color: Colors.black54),
+                          children: [
+                        TextSpan(
+                            text: "${message.posterName}: ",
+                            style:
+                                const TextStyle(fontWeight: FontWeight.bold)),
+                        TextSpan(text: message.text)
+                      ])),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 10.0),
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: Text(DateFormat('HH:mm').format(message.timePosted),
+                        style: const TextStyle(color: Colors.blue)),
+                  ),
+                ),
+              ],
+            )));
   }
 }
