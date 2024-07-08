734714d7549dd9945df51c13868a9a0e18ef68f6
marinawooden <mwoode@uw.edu>
2024-07-07 21:44:50 -0700

    sign up + login

--- flutter_app/lib/views/main_view.dart
+++ flutter_app/lib/views/main_view.dart
@@ -7,9 +7,11 @@ import 'package:groupiq_flutter/views/chat_info_view.dart';
 import 'package:groupiq_flutter/views/explore_view.dart';
 import 'package:groupiq_flutter/views/home_view.dart';
 import 'package:groupiq_flutter/views/login_view.dart';
+import 'package:groupiq_flutter/views/signup_view.dart';
 import 'package:groupiq_flutter/views/profile_self_view.dart';
 import 'package:groupiq_flutter/views/chat_view.dart';
 import 'package:pocketbase/pocketbase.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MainView extends StatefulWidget {
   late final PocketBase pb;
@@ -24,119 +26,164 @@ class MainView extends StatefulWidget {
 }
 
 class _MainViewState extends State<MainView> {
+  late final bool isLoggedIn;
+  final GlobalKey<NavigatorState> rootNavigatorKey =
+      GlobalKey<NavigatorState>();
+  late final GoRouter router;
+
   @override
-  Widget build(BuildContext context) {
-    final Size screenSize = MediaQuery.of(context).size;
-    final pb = widget.pb;
-    final isLoggedIn = pb.authStore.isValid;
-    final GlobalKey<NavigatorState> rootNavigatorKey =
-        GlobalKey<NavigatorState>();
+  void initState() {
+    super.initState();
+    isLoggedIn = widget.pb.authStore.isValid;
 
-    print(
-        'Device Width: ${screenSize.width}, Device Height: ${screenSize.height}');
-
-    final GoRouter router = GoRouter(
+    router = GoRouter(
       navigatorKey: rootNavigatorKey,
       initialLocation: isLoggedIn ? '/home' : '/login',
       routes: [
         ShellRoute(
-          // navigatorKey: _shellNavigatorKey,
           builder: (context, state, child) {
             return AppShell(child: child);
           },
-
-          /// Shell Routes
           routes: [
             GoRoute(
-                path:
-                    '/', // Made the default route the login view because it felt safer?
-                pageBuilder: (context, state) {
-                  return const CustomTransitionPage<void>(
-                      child: LoginView(),
-                      transitionsBuilder: _slideTransitionBuilder,
-                      transitionDuration: Duration(milliseconds: 300));
-                }),
+              path: '/',
+              pageBuilder: (context, state) {
+                return const CustomTransitionPage<void>(
+                  child: LoginView(),
+                  transitionsBuilder: _slideTransitionBuilder,
+                  transitionDuration: Duration(milliseconds: 300),
+                );
+              },
+            ),
             GoRoute(
-                path: '/home',
-                pageBuilder: (context, state) {
-                  return const CustomTransitionPage<void>(
-                      child: HomeView(),
-                      transitionsBuilder: _slideTransitionBuilder,
-                      transitionDuration: Duration(milliseconds: 300));
-                }),
+              path: '/home',
+              pageBuilder: (context, state) {
+                return const CustomTransitionPage<void>(
+                  child: HomeView(),
+                  transitionsBuilder: _slideTransitionBuilder,
+                  transitionDuration: Duration(milliseconds: 300),
+                );
+              },
+            ),
             GoRoute(
-                path: '/explore',
-                pageBuilder: (context, state) {
-                  return const CustomTransitionPage<void>(
-                      child: ExploreView(),
-                      transitionsBuilder: _slideTransitionBuilder,
-                      transitionDuration: Duration(milliseconds: 300));
-                }),
+              path: '/explore',
+              pageBuilder: (context, state) {
+                return const CustomTransitionPage<void>(
+                  child: ExploreView(),
+                  transitionsBuilder: _slideTransitionBuilder,
+                  transitionDuration: Duration(milliseconds: 300),
+                );
+              },
+            ),
             GoRoute(
-                path: '/profile',
-                pageBuilder: (context, state) {
-                  return CustomTransitionPage<void>(
-                      child: ProfileSelfView(),
-                      transitionsBuilder: _slideTransitionBuilder,
-                      transitionDuration: const Duration(milliseconds: 300));
-                }),
+              path: '/profile',
+              pageBuilder: (context, state) {
+                return CustomTransitionPage<void>(
+                  child: ProfileSelfView(),
+                  transitionsBuilder: _slideTransitionBuilder,
+                  transitionDuration: const Duration(milliseconds: 300),
+                );
+              },
+            ),
             GoRoute(
               path: '/chat',
               pageBuilder: (context, state) {
                 return const CustomTransitionPage<void>(
-                    child: ChatView(),
-                    transitionsBuilder: _slideTransitionBuilder,
-                    transitionDuration: Duration(milliseconds: 300));
+                  child: ChatView(),
+                  transitionsBuilder: _slideTransitionBuilder,
+                  transitionDuration: Duration(milliseconds: 300),
+                );
               },
             ),
             GoRoute(
               path: '/chat_info',
               pageBuilder: (context, state) {
                 return const CustomTransitionPage<void>(
-                    child: ChatInfoView(),
-                    transitionsBuilder: _slideTransitionBuilder,
-                    transitionDuration: Duration(milliseconds: 300));
+                  child: ChatInfoView(),
+                  transitionsBuilder: _slideTransitionBuilder,
+                  transitionDuration: Duration(milliseconds: 300),
+                );
               },
             ),
           ],
         ),
-
-        /// Full screen routes
-        GoRoute(path: '/login', builder: (context, state) => const LoginView()),
+        GoRoute(
+          path: '/login',
+          builder: (context, state) => const LoginView(),
+          routes: [
+            GoRoute(
+              path: 'signup',
+              builder: (context, state) => const SignUpView(),
+            ),
+          ],
+        ),
       ],
     );
 
+    print("InitState: isLoggedIn = $isLoggedIn");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print("Build MainView: isLoggedIn = $isLoggedIn");
+    final Size screenSize = MediaQuery.of(context).size;
+
     return ScreenUtilInit(
-        // this is according to our figma
-        designSize: const Size(428, 926),
-        // Whether to adapt the text according to the minimum of width and height
-        minTextAdapt: true,
-        // I think we set this to a list of widget names that we want to be responsive/rebuilt
-        //^ actually this might be the opposite. Per the docs:
-        // ark widget needs build only if:... responsiveWidgets does not contains widget name
-        responsiveWidgets: const [],
-        builder: (context, child) {
-          return MaterialApp.router(
-              title: 'Groupiq',
-              theme: ThemeData(
-                primaryColor: Colors.blue,
-                colorScheme: ColorScheme.fromSeed(
-                    seedColor: Colors.blue, brightness: Brightness.light),
-                floatingActionButtonTheme: const FloatingActionButtonThemeData(
-                  backgroundColor: Colors.blue,
+      designSize: const Size(428, 926),
+      minTextAdapt: true,
+      responsiveWidgets: const [],
+      builder: (context, child) {
+        return MaterialApp.router(
+          title: 'Groupiq',
+          theme: ThemeData(
+            textTheme: TextTheme(
+                bodyMedium: GoogleFonts.nunitoSans(
+                    fontSize: 15, fontWeight: FontWeight.w400),
+                displayLarge: GoogleFonts.nunitoSans(
+                    fontSize: 40, fontWeight: FontWeight.w700)),
+            primaryColor: Colors.blue,
+            colorScheme: ColorScheme.fromSeed(
+                seedColor: Colors.blue, brightness: Brightness.light),
+            floatingActionButtonTheme: const FloatingActionButtonThemeData(
+              backgroundColor: Colors.blue,
+            ),
+            inputDecorationTheme: const InputDecorationTheme(
+                filled: true,
+                fillColor: Color.fromRGBO(0, 115, 248, 1),
+                labelStyle: TextStyle(color: Colors.white),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white),
+                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
                 ),
-              ),
-              debugShowCheckedModeBanner: false,
-              routerConfig: router);
-        });
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white, width: 3.0),
+                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                ),
+                errorBorder: OutlineInputBorder(
+                    borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+                    borderSide:
+                        BorderSide(color: Color.fromARGB(255, 255, 185, 185))),
+                focusedErrorBorder: OutlineInputBorder(
+                    borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+                    borderSide: BorderSide(
+                        color: Color.fromRGBO(255, 185, 185, 1), width: 3.0)),
+                errorStyle:
+                    TextStyle(color: Color.fromARGB(255, 255, 185, 185))),
+          ),
+          debugShowCheckedModeBanner: false,
+          routerConfig: router,
+        );
+      },
+    );
   }
 }
 
 Widget _slideTransitionBuilder(
-    BuildContext context,
-    Animation<double> animation,
-    Animation<double> secondaryAnimation,
-    Widget child) {
+  BuildContext context,
+  Animation<double> animation,
+  Animation<double> secondaryAnimation,
+  Widget child,
+) {
   const begin = Offset(1.0, 0.0); // Slide from right
   const end = Offset.zero;
   const curve = Curves.ease;
